782ee14088c02cfc8898a0f6c8d38ede7a8a36c0
Nanle Paul <paulnanle611@gmail.com>
2016-12-01 23:50:27 +0100

    Update PositiveIntegers.java

--- positiveintegers/PositiveIntegers.java
+++ positiveintegers/PositiveIntegers.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package positiveintegers;
 
 /**
@@ -11,12 +7,8 @@ package positiveintegers;
  */
 import java.util.Scanner;
 public class PositiveIntegers {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the number of numbers which you want to input!");
         int numcases = Integer.parseInt(in.nextLine());
